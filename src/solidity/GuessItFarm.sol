0faf998f2d10a777b84066ef954a13ef389cc276
Selim Iskender <siskender@outlook.com>
2021-04-26 14:49:58 +0200

    Updated contracts to include all functionality

--- src/solidity/GuessItFarm.sol
+++ src/solidity/GuessItFarm.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./GuessItToken.sol";
-import "./IPancakeRouter02.sol";
-import "./IPancakePair.sol";
+import "./GuessItRewards.sol";
 
-contract GuessItFarm is Ownable, ReentrancyGuard {
+contract GuessItFarm is Ownable, AccessControl, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     event Deposit(address indexed user, uint indexed pid, uint amount);
@@ -18,6 +18,7 @@ contract GuessItFarm is Ownable, ReentrancyGuard {
 
     struct UserInfo {
         uint amount; // How many tokens the user has provided.
+        uint lockedUntil; // Until when these tokens are locked.
         uint rewardDebt; // Reward debt. See explanation below.
         //
         // We do some fancy math here. Basically, any point in time, the amount of RADSs
@@ -41,21 +42,24 @@ contract GuessItFarm is Ownable, ReentrancyGuard {
         uint depositFee; // Deposit fee on this pool, in per mille (percentage) 
     }
 
-    IPancakeRouter02 public immutable pancakeRouter;
     PoolInfo[] public poolInfo; // Info of each pool.
     mapping(uint => mapping(address => UserInfo)) public userInfo; // Info of each user that stakes LP tokens.
-    uint public immutable nativePerBlock = 1e4 ether; // native tokens minted per block => 10,000
+    uint public immutable minNativePerBlock = 5000 ether; // min native tokens minted per block => 5000
+    uint public immutable maxNativePerBlock = 10000 ether; // max native tokens minted per block => 10000
     uint public totalAllocationPoints = 0; // Total allocation points. Must be the sum of all allocation points in all pools.
     uint public immutable startBlock;
-    address public native;
+    uint public immutable minLockDays = 0;
+    uint public immutable maxLockDays = 30;
+    GuessItToken immutable public native;
+    GuessItRewards immutable public rewards;
 
     uint private _nativeShareMultiplier = 1e12;
     uint private _perMille = 1000; // 100%
 
-    constructor(address _pancakeRouter, address _native) {
-        pancakeRouter = IPancakeRouter02(_pancakeRouter);
-        native = _native;
-        startBlock = block.number;        
+    constructor(address _native, address payable _rewards, uint _startBlock) {
+        native = GuessItToken(_native);
+        rewards = GuessItRewards(_rewards);        
+        startBlock = _startBlock;
     }
 
     function getPoolInfo() external view returns (PoolInfo[] memory) {
@@ -95,20 +99,27 @@ contract GuessItFarm is Ownable, ReentrancyGuard {
     // Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint _from, uint _to) public view returns (uint)
     {
-        if (GuessItToken(native).totalSupply() >= GuessItToken(native).cap()) {
+        if (native.totalSupply() >= native.cap()) {
             return 0;
         }
         return _to - _from;
     }
 
+    function getNativePerBlock(uint _pid, address _user) public view returns (uint) {
+        UserInfo storage user = userInfo[_pid][_user];
+        uint extraNativePerBlock = maxNativePerBlock - minNativePerBlock;
+        return minNativePerBlock + extraNativePerBlock * user.lockedUntil / maxLockDays;
+    }
+
     // View function to see pending [native] on frontend.
-    function pendingNative(uint _pid, address _user) external view returns (uint) {
+    function getPendingNative(uint _pid, address _user) external view returns (uint) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
         uint accumlatedNativePerShare = pool.accumlatedNativePerShare;
         uint amount = IERC20(pool.token).balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && amount != 0) {
             uint multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint nativePerBlock = getNativePerBlock(_pid, _user);
             uint nativeReward = multiplier * nativePerBlock * pool.allocationPoints / totalAllocationPoints;
             accumlatedNativePerShare += nativeReward * _nativeShareMultiplier / amount;
         }
@@ -138,104 +149,97 @@ contract GuessItFarm is Ownable, ReentrancyGuard {
         if (multiplier <= 0) {
             return;
         }
-        uint mintableTokensLeft = GuessItToken(native).cap() - GuessItToken(native).totalSupply();
+        uint mintableTokensLeft = native.cap() - native.totalMinted();
+        uint nativePerBlock = getNativePerBlock(_pid, _msgSender());
         uint tokensToMint = mintableTokensLeft >= nativePerBlock ? nativePerBlock : mintableTokensLeft;
         uint nativeReward = multiplier * tokensToMint * pool.allocationPoints / totalAllocationPoints;        
-        GuessItToken(native).mint(address(this), nativeReward);
+        native.mint(address(this), nativeReward);
 
         pool.accumlatedNativePerShare += nativeReward * _nativeShareMultiplier / amount;
         pool.lastRewardBlock = block.number;
     }
    
     // Deposit tokens for [native] allocation.
-    function deposit(uint _pid, uint _amount) external nonReentrant {       
+    function deposit(uint _pid, uint _amount, uint _locked) external nonReentrant {       
         PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
+        UserInfo storage user = userInfo[_pid][_msgSender()];
         updatePool(_pid);
-
-        if (user.amount > 0) {
+       
+        if(user.amount > 0) {           
             uint pending = user.amount * pool.accumlatedNativePerShare / _nativeShareMultiplier - user.rewardDebt;
-            if (pending > 0) {
-                safeNativeTransfer(msg.sender, pending);
+            if(pending > 0) {
+                safeNativeTransfer(_msgSender(), pending);
             }
         }
         if (_amount > 0) {
-            IERC20(pool.token).safeTransferFrom(address(msg.sender), address(this), _amount);
+            IERC20(pool.token).safeApprove(address(this), 0);
+            IERC20(pool.token).safeIncreaseAllowance(address(this), _amount);
+            IERC20(pool.token).safeTransferFrom(_msgSender(), address(this), _amount);
+            IERC20(pool.token).safeApprove(address(this), 0);
             if (pool.depositFee > 0) {
                 uint depositFee = _amount / _perMille * pool.depositFee;
                 user.amount += _amount - depositFee;
-                _distributeDepositFee(pool, depositFee); // distribute the deposit fee to the rewards pool
+                user.lockedUntil = max(user.lockedUntil, block.timestamp + min(_locked, maxLockDays) * 1 days); // look also at an existing lock, before adding to the lock
+                _distributeDepositFee(pool, depositFee);
             } else {                   
                 user.amount += _amount;
             }
         }
         user.rewardDebt = user.amount * pool.accumlatedNativePerShare / _nativeShareMultiplier;
-        emit Deposit(msg.sender, _pid, _amount);
+        emit Deposit(_msgSender(), _pid, _amount);
+    }
+    
+    function max(uint a, uint b) private pure returns (uint) {
+        return a > b ? a : b;
+    }
+    
+    function min(uint a, uint b) private pure returns (uint) {
+        return a < b ? a : b;
     }
 
     // Withdraw tokens
     function withdraw(uint _pid, uint _amount) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(user.amount > 0, "Nothing to withdraw");
-        require(user.amount >= _amount, "Withdraw not allowed");
+        UserInfo storage user = userInfo[_pid][_msgSender()];
+        require(user.lockedUntil <= block.timestamp, "GuessItFarm: withdraw not allowed");
+        require(user.amount > 0, "GuessItFarm: nothing to withdraw");
+        require(user.amount >= _amount, "GuessItFarm: withdraw not allowed");
 
         updatePool(_pid);
         uint pending = user.amount * pool.accumlatedNativePerShare / _nativeShareMultiplier - user.rewardDebt;
         if(pending > 0) {
-            safeNativeTransfer(msg.sender, pending);
+            safeNativeTransfer(_msgSender(), pending);
         }
         if(_amount > 0) {
             user.amount -= _amount;            
-            IERC20(pool.token).safeTransfer(address(msg.sender), _amount);
+            IERC20(pool.token).safeTransfer(address(_msgSender()), _amount);
         }
         user.rewardDebt = user.amount * pool.accumlatedNativePerShare / _nativeShareMultiplier;
-        emit Withdraw(msg.sender, _pid, _amount);
+        emit Withdraw(_msgSender(), _pid, _amount);
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
     function emergencyWithdraw(uint _pid) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
+        UserInfo storage user = userInfo[_pid][_msgSender()];
         uint amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
-        IERC20(pool.token).safeTransfer(address(msg.sender), amount);
-        emit EmergencyWithdraw(msg.sender, _pid, amount);
+        IERC20(pool.token).safeTransfer(address(_msgSender()), amount);
+        emit EmergencyWithdraw(_msgSender(), _pid, amount);
     }
 
     // Safe [native] transfer function, just in case if rounding error causes pool to not have enough
     function safeNativeTransfer(address _to, uint _nativeAmount) internal {
-        uint nativeBalance = GuessItToken(native).balanceOf(address(this));
+        uint nativeBalance = native.balanceOf(address(this));
         if (_nativeAmount > nativeBalance) {
-            GuessItToken(native).transfer(_to, nativeBalance);
+            native.transfer(_to, nativeBalance);
         } else {
-            GuessItToken(native).transfer(_to, _nativeAmount);
+            native.transfer(_to, _nativeAmount);
         }
     }
 
     function _distributeDepositFee(PoolInfo storage pool, uint depositFee) private {
-        address rewardsAddr = GuessItToken(native).rewards.address;
-        if(pool.isPair) {
-            IPancakePair pair = IPancakePair(pool.token);
-            address token0 = pair.token0();
-            address token1 = pair.token1();
-            (uint amountA, uint amountB) = pancakeRouter.removeLiquidity(token0, token1, depositFee, 0, 0, address(this), block.timestamp);
-            _swapTokensForBnb(token0, rewardsAddr, amountA);
-            _swapTokensForBnb(token1, rewardsAddr, amountB);
-        } else {
-            _swapTokensForBnb(rewardsAddr, pool.token, depositFee);
-        }
-    }
-    
-    function _swapTokensForBnb(address _token, address _to, uint _amount) private {
-        // generate the pancake pair path of token -> wbnb
-        address[] memory path = new address[](2);
-        path[0] = _token;
-        path[1] = pancakeRouter.WETH();
-
-        // make the swap
-        IERC(_token).approve(address(pancakeRouter), _amount);
-        pancakeRouter.swapExactTokensForETHSupportingFeeOnTransferTokens(_amount, 0, path, _to, block.timestamp);
+        rewards.tokenReceive(pool.token, pool.isPair, _msgSender(), depositFee);
     }
 }
--- src/solidity/GuessItToken.sol
+++ src/solidity/GuessItToken.sol
@@ -1,16 +1,17 @@
 ﻿// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./IPancakeRouter02.sol";
 import "./IPancakeFactory.sol";
 import "./GuessItRewards.sol";
 
-contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, ReentrancyGuard {
+contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, AccessControl, Ownable, ReentrancyGuard {
     
     event GameStarted();
     event Guessed(address _from, string _solution, bool _guessed);
@@ -18,7 +19,7 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
     event Withdrawn(address _to, uint amount);
 
     enum GameState { Created, LiquidityAdded, Started, Finished }
-    enum UserState { Claimed, Withdrawn }
+    enum UserState { Withdrawn }
 
     modifier inGameState(GameState state) {
         require(_state == state, "GuessItToken: invalid game state");
@@ -30,13 +31,8 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         _;
     }
 
-    modifier inUserState(UserState state) {
-        require(_userStates[msg.sender] == state, "GuessItToken: invalid user state");
-        _;
-    }
-
     modifier notInUserState(UserState state) {
-        require(_userStates[msg.sender] != state, "GuessItToken: invalid user state");
+        require(_userStates[_msgSender()] != state, "GuessItToken: invalid user state");
         _;
     }
 
@@ -47,6 +43,7 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         bool finished;
     }
     
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     GuessItRewards public immutable rewards;
     IPancakeRouter02 public immutable pancakeRouter;
     uint public guessPrice;
@@ -54,35 +51,38 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
     uint public transferPercentage = 980; //initial burn percentage of the transfer, in per mille
     uint public rewardsPercentage = 500; //initial rewards percentage, in per mille
 
+    uint private _totalMinted;
     mapping (address => bool) private _isExcludedFromFee;
     uint private _perMille = 1000; // 100%
     uint private _snapShotId;
     Game private _game;
-    mapping (address => uint) private _pendingWithdrawals;
     mapping (address => bool) private _guessers;
     GameState private _state;
     mapping (address => UserState) private _userStates;
 
-    constructor(address _pancakeRouter, address _dev) ERC20Capped(1e11 ether) ERC20("GuessIt Token", "GSSIT") {
+    constructor(address _pancakeRouter, address payable _rewards) ERC20Capped(1e11 ether) ERC20("GuessIt Token", "GSSIT") {
         pancakeRouter = IPancakeRouter02(_pancakeRouter);
-        rewards = new GuessItRewards(_dev);
+        rewards = GuessItRewards(_rewards);
 
-        _isExcludedFromFee[owner()] = true;
-        _isExcludedFromFee[address(this)] = true;
+        excludeFromFee(owner());
+        excludeFromFee(address(this));
+        excludeFromFee(_rewards);
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
     
-    function addLiquidity() external payable onlyOwner() inGameState(GameState.Created) {
+    function addLiquidity(address _addr) external payable onlyOwner inGameState(GameState.Created) {
         require(msg.value == 20 ether, "GuessItToken: not enough liquidity provided");
         mint(address(this), 1e7 ether); // preminted 0.01% of tokens to create liquidity pairs
         approve(address(pancakeRouter), 1e7 ether); // approve the router to spend tokens to create liquidity pairs        
-        pancakeRouter.addLiquidityETH{value:msg.value}(address(this), 1e7 ether, 1e7 ether, msg.value, getDev(), block.timestamp + 1 minutes);
+        pancakeRouter.addLiquidityETH{value:msg.value}(address(this), 1e7 ether, 1e7 ether, msg.value, _addr, block.timestamp + 1 minutes);
     }
 
     function excludeFromFee(address account) public onlyOwner {
         _isExcludedFromFee[account] = true;
     }
     
-    function includeInFee(address account) public onlyOwner {
+    function includeInFee(address account) external onlyOwner {
         _isExcludedFromFee[account] = false;
     }
 
@@ -90,7 +90,7 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         return _isExcludedFromFee[account];
     }
 
-    function newGame(Game calldata game) external onlyOwner() inGameState(GameState.LiquidityAdded) {
+    function newGame(Game calldata game) external onlyOwner inGameState(GameState.LiquidityAdded) {
         require(game.puzzle != "", "GuessItToken: puzzle should be provided");
         require(game.solutions.length > 0, "GuessItToken: solutions should be provided");
 
@@ -107,7 +107,7 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         return _game;
     }
 
-    function setPrices(uint _guessPrice) external onlyOwner() inGameState(GameState.Started) {
+    function setPrices(uint _guessPrice) external onlyOwner inGameState(GameState.Started) {
         guessPrice = _guessPrice;
         emit PriceUpdated(_guessPrice);
     }
@@ -116,35 +116,39 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         require(_amount == guessPrice, "GuessItToken: guessing price conditions not met");
         uint amountForRewards = _amount * rewardsPercentage / _perMille;
         uint amountToBurn = _amount - amountForRewards;        
-        _burn(msg.sender, amountToBurn);
-        _swapTokensForBnb(address(rewards), amountForRewards);
+        if(amountToBurn > 0) {
+            _burn(_msgSender(), amountToBurn);
+        }
+        if(amountForRewards > 0) {
+            rewards.tokenReceive(address(this), false, _msgSender(), amountForRewards);
+        }
 
         uint solutions = _game.solutions.length;
         bytes32 hashedSolution = sha256(abi.encodePacked(_toLower(solution)));
         for(uint i = 0; i < solutions; i++) {
             if(_game.solutions[i] == hashedSolution) {                
-                _game.finished = _guessers[msg.sender] = true;
+                _game.finished = _guessers[_msgSender()] = true;
                 _snapShotId = _snapshot();
                 _state = GameState.Finished;
-                emit Guessed(msg.sender, solution, true);                
+                emit Guessed(_msgSender(), solution, true);                
                 return true;
-            }
+            } 
         }
         
-        _game.finished = _guessers[msg.sender] = false;
-        emit Guessed(msg.sender, solution, false);
+        _game.finished = _guessers[_msgSender()] = false;
+        emit Guessed(_msgSender(), solution, false);
         return false;
     }
 
-    function claimableRewards() public view inGameState(GameState.Finished) notInUserState(UserState.Withdrawn) returns (uint) {
+    function claimableRewards() public view inGameState(GameState.Finished)  returns (uint) {
         require(_game.started, "GuessItToken: there is no active game");
         if(!_game.finished) {
             return 0;
         }
 
-        bool isGuesser = _guessers[msg.sender];
+        bool isGuesser = _guessers[_msgSender()];
         uint totalSupply = totalSupplyAt(_snapShotId);
-        uint userSupply = balanceOfAt(msg.sender, _snapShotId);
+        uint userSupply = balanceOfAt(_msgSender(), _snapShotId);
 
         uint guesserRewards = address(rewards).balance * guesserPercentage / _perMille;
         uint participationRewards = 0;
@@ -155,26 +159,19 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         return isGuesser ? guesserRewards + participationRewards : participationRewards;
     }
 
-    function claimRewards() external inGameState(GameState.Finished) notInUserState(UserState.Withdrawn) {
-        uint rewardsToClaim = claimableRewards();
-        require(rewardsToClaim > 0, "GuessItToken: there are no rewards to claim");
-    
-        _pendingWithdrawals[msg.sender] = rewardsToClaim;
-        _userStates[msg.sender] = UserState.Claimed;
-    }
+    function withdraw() external inGameState(GameState.Finished) notInUserState(UserState.Withdrawn) nonReentrant {
+        uint amount = claimableRewards();
+        require(amount > 0, "GuessItToken: there are no rewards to claim");
 
-    function withdraw() external inGameState(GameState.Finished) inUserState(UserState.Claimed) nonReentrant {
-        uint amount = _pendingWithdrawals[msg.sender];
         require(amount > 0, "GuessItToken: there is nothing to withdraw");        
-        _pendingWithdrawals[msg.sender] = 0;
-        _userStates[msg.sender] = UserState.Withdrawn;
-        GuessItRewards(rewards).transferRewards(payable(msg.sender), amount);
-        emit Withdrawn(msg.sender, amount);        
+        _userStates[_msgSender()] = UserState.Withdrawn;
+        rewards.transferRewards(payable(_msgSender()), amount);        
+        emit Withdrawn(_msgSender(), amount);        
     }
 
     function setTransferPercentage(uint _transferPercentage) external onlyOwner {
         require(_transferPercentage >= 950, "GuessItToken: invalid percentage");
-        require(_transferPercentage <= 1000, "GuessItToken: invalid percentage");
+        require(_transferPercentage <= 990, "GuessItToken: invalid percentage");
         transferPercentage = _transferPercentage;
     }
 
@@ -190,20 +187,28 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
         guesserPercentage = _guesserPercentage;
     }
 
-    function setDev(address _dev) public onlyOwner {
-        rewards.setDev(_dev);
-    }
-
-    function getDev() public view returns(address) {
-        return rewards.getDev();
-    }
-
-    function mint(address _to, uint _amount) public onlyOwner {
+    function mint(address _to, uint _amount) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "GuessItToken: caller is not a minter");
         _mint(_to, _amount);
     }
 
+    function totalMinted() public view returns (uint) {
+        return _totalMinted;
+    }
+
+    // Returns for the sender the viewable puzzle share (in permille).
+    function getViewablePuzzleShare() external view returns (uint) {
+        if(_game.finished) {
+            return _perMille;
+        }
+
+        uint userSupply = balanceOf(_msgSender());
+        uint maxSupply = cap() - (totalMinted() - totalSupply());
+        return userSupply * _perMille / maxSupply;
+    }
+
     function _transfer(address _sender, address _recipient, uint _amount) internal override {
-        if(_isExcludedFromFee[_sender] || _isExcludedFromFee[_recipient]) {
+        if(isExcludedFromFee(_sender) || isExcludedFromFee(_recipient)) {
             super._transfer(_sender, _recipient, _amount);
             return;
         }
@@ -216,28 +221,18 @@ contract GuessItToken is ERC20Snapshot, ERC20Burnable, ERC20Capped, Ownable, Ree
             _burn(_sender, amountToBurn);
         }
         if(amountForRewards > 0) {
-            _swapTokensForBnb(address(rewards), amountForRewards);
+            rewards.tokenReceive(address(this), false, _msgSender(), amountForRewards);
         }
         super._transfer(_sender, _recipient, amountToTransfer);
     }
 
     function _mint(address account, uint amount) internal override(ERC20Capped, ERC20) {
         ERC20Capped._mint(account, amount);
+        _totalMinted += amount;
     }
 
     function _beforeTokenTransfer(address from, address to, uint tokenId) internal override(ERC20Snapshot, ERC20) {
         super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _swapTokensForBnb(address _to, uint _amount) private {
-        // generate the pancake pair path of token -> wbnb
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = pancakeRouter.WETH();
-
-        // make the swap
-        approve(address(pancakeRouter), _amount);
-        pancakeRouter.swapExactTokensForETHSupportingFeeOnTransferTokens(_amount, 0, path, _to, block.timestamp);
     }
 
     function _toLower(string calldata str) private pure returns (string memory) {
