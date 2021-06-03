c4c5e71959baa581d70f815fcc2eaa388d69a3c1
Selim Iskender <siskender@outlook.com>
2021-06-03 17:55:50 +0200

    Removed wbnb from rewards contract.

--- src/main/contracts/GuessItRewards.sol
+++ src/main/contracts/GuessItRewards.sol
@@ -3,22 +3,18 @@ pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "./IWETH.sol";
 
 contract GuessItRewards is Ownable, AccessControl {
     event RewardsReceived(uint _amount);
 
     bytes32 public constant TRANSFER_ROLE = keccak256("TRASNFER_ROLE");
     address public immutable dev;
-    address public immutable WBNB;
     uint public immutable devPercentage = 500; //50%, percentage of the rewards distributed to the dev address, in per mille
     
     uint private _perMille = 1000; // 100%
 
-    constructor(address _dev, address _WBNB) {
+    constructor(address _dev) {
         dev = _dev;
-        WBNB = _WBNB;
-
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
